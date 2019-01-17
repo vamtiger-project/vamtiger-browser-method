c2e0a85fc0432c5821442222ebfc0be464f92bfc
James-Alex Matthews <vamtiger@vamtiger.com>
2019-01-17 17:58:46 +0200

    vamtiger-node-typescript-commit

--- source/vamtiger-browser-method.ts
+++ source/vamtiger-browser-method.ts
@@ -2,11 +2,13 @@ export * from './load-ts-lib';
 import loadScript from './load-script';
 import loadShadowStylesheet from './load-shadow-stylesheet';
 import defineCustomElement from './define-custom-element';
+import pause from './pause';
 
 const VamtigerBrowserMethod = {
     loadScript,
     loadShadowStylesheet,
-    defineCustomElement
+    defineCustomElement,
+    pause
 };
 
 try {
