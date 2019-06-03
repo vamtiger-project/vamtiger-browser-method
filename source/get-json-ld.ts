fa8816d00f2c6f5e353858ecd935778a5d366d3f
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-03 23:54:27 +0200

    vamtiger-node-typescript-commit

--- source/get-json-ld.ts
+++ source/get-json-ld.ts
@@ -7,26 +7,7 @@ import queue from './queue';
 import loadWebComponentData from './load-web-component-data';
 import getWebComponentData from './get-web-component-data';
 
-export default function (params: IGetJsonLd) {return new Promise(async (resolve: WebComponentDataResolve, reject: ErrorResolve) => {
-    const { jsonLd: url } = params;
-    const jsonLd = await getJsonLd(params)
-    const queueParams = !jsonLd && {
-        key: url,
-        resolve,
-        reject
-    };
-
-    if (jsonLd) {
-        resolve(jsonLd);
-    } else if (queueParams) {
-        queue(queueParams);
-
-        await loadWebComponentData({ url });
-        getWebComponentData({ key: url });
-    }
-})}
-
-export function getJsonLd({ jsonLd: url }: IGetJsonLd) {return new Promise(async (resolve: WebComponentDataResolve, reject: ErrorResolve) => {
+export default function ({ jsonLd: url }: IGetJsonLd) {return new Promise(async (resolve: WebComponentDataResolve, reject: ErrorResolve) => {
     const queueParams = {
         key: url,
         resolve,
@@ -34,6 +15,12 @@ export function getJsonLd({ jsonLd: url }: IGetJsonLd) {return new Promise(async
     };
 
     queue(queueParams);
+
+    try {
+        await loadWebComponentData({ url });
+    } catch(error) {
+        console.error(error);
+    }
 
     getWebComponentData({ key: url });
 })}
